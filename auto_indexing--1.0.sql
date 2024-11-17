4ff2eed7a1ad1649980c1aad41b5f3f9837e28cc
theotimeflichy <tronaidev@gmail.com>
2024-11-17 16:03:27 +0530

    Link query_log table to plugin.

--- auto_indexing--1.0.sql
+++ auto_indexing--1.0.sql
@@ -5,3 +5,11 @@ AS 'auto_indexing'
 SELECT auto_indexing();
 
 COMMENT ON EXTENSION auto_indexing IS 'Auto Indexing extension for Postgresql. Project for IIT Bombay, CS631.';
+
+CREATE TABLE query_log (
+                           query_type VARCHAR(50) NOT NULL,
+                           table_name VARCHAR(255) NOT NULL,
+                           clause_type VARCHAR(50),
+                           clause_name TEXT,
+                           log_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
